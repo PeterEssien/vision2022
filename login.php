1c8dfb908247fb26247104a121d623c2c481f536
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2021-10-12 08:24:25 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Login</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts1/icomoon/style.css">
+
+    <link rel="stylesheet" href="css1/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css1/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css1/style.css">
+
+    <title>Login Goldman Sachs</title>
+    	  <link href="../img/assets/favicon.ico" rel="icon" type="image/png" />
+  </head>
+  <body>
+  
 <?php
 require('db.php');
 session_start();
@@ -35,15 +50,51 @@ and password='".md5($password)."'";
 	}
     }else{
 ?>
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
-<input type="password" name="password" placeholder="Password" required />
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
-</div>
-<?php } ?>
-</body>
+
+  <div class="d-lg-flex half">
+    <div class="bg order-1 order-md-2" style="background-image: url('images1/bg_1.jpg');"></div>
+    <div class="contents order-2 order-md-1">
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-7">
+            <h3>Login to <strong>Goldman Sachs</strong></h3>
+            <p class="mb-4">You need to login in order to get access to your dashboard.</p>
+            <form action="" method="post" name="login">
+              <div class="form-group first">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" placeholder="Username" id="username" name="username">
+              </div>
+              <div class="form-group last mb-3">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" placeholder="Password" id="password" name="password">
+              </div>
+              
+              <div class="d-flex mb-5 align-items-center">
+                <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
+                  <input type="checkbox" checked="checked"/>
+                  <div class="control__indicator"></div>
+                </label>
+                <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
+              </div>
+
+              <input name="submit" type="submit" value="Log In" class="btn btn-block btn-primary">
+
+            </form>
+            <p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+    
+    
+
+    <script src="js1/jquery-3.3.1.min.js"></script>
+    <script src="js1/popper.min.js"></script>
+    <script src="js1/bootstrap.min.js"></script>
+    <script src="js1/main.js"></script>
+  </body>
 </html>
