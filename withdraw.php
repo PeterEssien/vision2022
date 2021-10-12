2a3b445d71911991898d6ba3ccc23122f463069d
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2021-10-12 14:36:39 -0500

    Update and rename details.php to withdraw.php

---
+++ withdraw.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts1/icomoon/style.css">
+
+    <link rel="stylesheet" href="css1/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css1/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css1/style.css">
+
+    <title>Withdraw | Goldman Sachs</title>
+    	  <link href="../img/assets/favicon.ico" rel="icon" type="image/png" />
+  </head>
+  <body>
+  
+
+
+  <div class="d-lg-flex half">
+    <div class="bg order-1 order-md-2" style="background-image: url('images1/bg_1.jpg');"></div>
+    <div class="contents order-2 order-md-1">
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-7">
+            <h3>Withdraw Funds | <strong>Goldman Sachs</strong></h3>
+            <p class="mb-4">Please fill in the details below accurately to withdraw your funds.</p>
+            <form action="" method="post" name="withdraw">
+              <div class="form-group first">
+                <label for="username">Amount To Withdraw</label>
+                <input type="text" class="form-control" placeholder="Amount" id="amount" name="amount" required>
+              </div>
+
+               <div class="form-group first">
+                <label for="username">Wallet Address</label>
+                <input type="email" class="form-control" placeholder="Address" id="wallet" name="wallet" required>
+              </div>
+
+              <div class="form-group last mb-3">
+                <label for="password">Full Name</label>
+                <input type="password" class="form-control" placeholder="Name" id="name" name="name" required>
+              </div>
+            
+
+              <input name="submit" type="submit" value="Withdraw" class="btn btn-block btn-primary">
+
+            </form>
+        
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
+    
+    
+
+    <script src="js1/jquery-3.3.1.min.js"></script>
+    <script src="js1/popper.min.js"></script>
+    <script src="js1/bootstrap.min.js"></script>
+    <script src="js1/main.js"></script>
+ 
+</body>
+</html>
