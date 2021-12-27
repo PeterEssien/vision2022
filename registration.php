6f8e5231a27fb5488dae34df16e44b797d5522ea
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2021-12-27 09:49:51 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -45,10 +45,6 @@ VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
     }else{
 ?>
 
-  <div class="d-lg-flex half">
-    <div class="bg order-1 order-md-2" style="background-image: url('images1/bg_1.jpg');"></div>
-    <div class="contents order-2 order-md-1">
-
       <div class="container">
         <div class="row align-items-center justify-content-center">
           <div class="col-md-7">
@@ -79,10 +75,6 @@ VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
           </div>
         </div>
       </div>
-    </div>
-
-    
-  </div>
     
     
 
