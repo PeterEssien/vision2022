fd447520faafbb3a18a47f448503774115724443
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2022-04-18 23:23:28 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -12,7 +12,7 @@ include("auth.php");
 
     <meta charset="UTF-8">
 
-    <!--<title>Gold-dyx Dashboard</title>-->
+    <!--<title>Gold-dyx | Dashboard</title>-->
 
     <link rel="stylesheet" href="style.css">
 
@@ -237,184 +237,11 @@ include("auth.php");
 
       <div class="sales-boxes">
 
-        <div class="recent-sales box">
 
-          <div class="title">Top Investors Gain</div>
-
-          <div class="sales-details">
-
-            <ul class="details">
-
-              <li class="topic">Date</li>
-
-             <li><a href="#">18 Feb 2022</a></li>
-
-             <li><a href="#">04 Feb 2021</a></li>
-
-              <li><a href="#">28 Jan 2022</a></li>
-
-              <li><a href="#">21 Jan 2022</a></li>
-
-
-             <li><a href="#">31 Dec 2021</a></li>
-
-             <li><a href="#">29 Dec 2021</a></li>
-
-              <li><a href="#">01 Dec 2021</a></li>
-
-              <li><a href="#">18 Nov 2021</a></li>
-
-
-              <li><a href="#">13 Oct 2021</a></li>
-
-              <li><a href="#">11 Oct 2021</a></li>
-
-              <li><a href="#">08 Oct 2021</a></li>
-
-              <li><a href="#">31 Sep 2021</a></li>
-
-              <li><a href="#">18 Sep 2021</a></li>
-
-              <li><a href="#">01 Sep 2021</a></li>
-
-              <li><a href="#">22 Aug 2021</a></li>
-
-              <li><a href="#">16 Aug 2021</a></li>
-
-              <li><a href="#">02 Aug 2021</a></li>
-
-            </ul>
-
-            <ul class="details">
-
-            <li class="topic">Customer</li>
-
-            <li><a href="#">Godfrey Hanson</a></li>
-
-            <li><a href="#">Maggi Shroom</a></li>
-
-            <li><a href="#">Paul Denim</a></li>
-
-            <li><a href="#">Anthony Sigurdsson</a></li>
-
-
-            <li><a href="#">Mary Jeffery</a></li>
-
-            <li><a href="#">Jadyn Matt</a></li>
-
-            <li><a href="#">Williams Parker</a></li>
-
-            <li><a href="#">Helena Dunn</a></li>
-
-
-            <li><a href="#">Alex Ocean</a></li>
-
-            <li><a href="#">David Mart</a></li>
-
-            <li><a href="#">Roe Parter</a></li>
-
-            <li><a href="#">Diana Penty</a></li>
-
-            <li><a href="#">Martin Paw</a></li>
-
-            <li><a href="#">Regan Malt</a></li>
-
-            <li><a href="#">Aiana Lexa</a></li>
-
-            <li><a href="#">Rexel Mags</a></li>
-
-             <li><a href="#">Tiana Loths</a></li>
-
-          </ul>
-
-          <ul class="details">
-
-            <li class="topic">Status</li>
-
-            <li><a href="#">Pending</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-             <li><a href="#">Paid</a></li>
-
-            <li><a href="#">Paid</a></li>
-
-          </ul>
-
-          <ul class="details">
-
-            <li class="topic">Amount</li>
-
-           <li><a href="#">$6,168.33</a></li>
-
-           <li><a href="#">$13,368.87</a></li>
-
-            <li><a href="#">$3,342.26</a></li>
-
-            <li><a href="#">$43,251.32</a></li>
-
-
-          <li><a href="#">$691,168.33</a></li>
-
-           <li><a href="#">$195,368.87</a></li>
-
-            <li><a href="#">$10,342.26</a></li>
-
-            <li><a href="#">$34,251.32</a></li>
-
-
-            <li><a href="#">$205,000.98</a></li>
-
-            <li><a href="#">$214,543.55</a></li>
-
-            <li><a href="#">$415,871.88</a></li>
-
-            <li><a href="#">$180,549.66</a></li>
-
-            <li><a href="#">$56,026.56</a></li>
-
-            <li><a href="#">$15,052.95</a></li>
-
-            <li><a href="#">$112,221.33</a></li>
-
-             <li><a href="#">$23,180.53</a></li>
-
-             <li><a href="#">$68,719.52</a></li>
-
-          </ul>
-
-          </div>
-
+      
           <div class="button">
 
-            <a href="https://explorer.lbtc.io/">Latest</a>
+            <a href="https://explorer.lbtc.io/">View on blockchain</a>
 
           </div>
 
