30a93501bb86fa9c903de2bd71764c0d8135a180
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-03-14 08:23:52 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -36,7 +36,7 @@ if (isset($_REQUEST['email'])){
 	$password = mysqli_real_escape_string($con,$password);
 	$trn_date = date("Y-m-d H:i:s");
         $query = "INSERT into `users` (username, password, email, trn_date)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+VALUES ('$username', '$password', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
         if($result){
             echo "<div class='form'> <h3>You are registered successfully.</h3>
