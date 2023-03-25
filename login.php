da5bc785b7b716aae3d410dbb7530fe9df2efab9
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-03-26 00:24:36 +0100

    Update login.php

--- login.php
+++ login.php
@@ -62,7 +62,7 @@ and password='$password'";
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <div class="brand-logo">
-                <img src="../../images/logo.svg" alt="logo">
+                <img src="img/logo-w-t-light.png" alt="logo">
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="fw-light">Sign in to continue.</h6>
