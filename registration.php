a6b465af67c235e6b3b13394b0ff93d957de16ce
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-03-26 00:50:56 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,40 +1,43 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Golddyx - Create Account</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../../vendors/feather/feather.css">
+  <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../../vendors/typicons/typicons.css">
+  <link rel="stylesheet" href="../../vendors/simple-line-icons/css/simple-line-icons.css">
+  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../img/logo-w-t-light.png" />
+</head>
 
-    <link rel="stylesheet" href="fonts1/icomoon/style.css">
-
-    <link rel="stylesheet" href="css1/owl.carousel.min.css">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css1/bootstrap.min.css">
-    
-    <!-- Style -->
-    <link rel="stylesheet" href="css1/style.css">
-    
-    <title>Register | Gold-dyx</title>
-    	  <link href="../img/assets/favicon.ico" rel="icon" type="image/png" />
-  </head>
-  <body>
-  
+<body>
+ 
 <?php
 require('db.php');
 // If form submitted, insert values into the database.
 
 if (isset($_REQUEST['email'])){
         // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
+  $username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username); 
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
-	$trn_date = date("Y-m-d H:i:s");
+  $username = mysqli_real_escape_string($con,$username); 
+  $email = stripslashes($_REQUEST['email']);
+  $email = mysqli_real_escape_string($con,$email);
+  $password = stripslashes($_REQUEST['password']);
+  $password = mysqli_real_escape_string($con,$password);
+  $trn_date = date("Y-m-d H:i:s");
         $query = "INSERT into `users` (username, password, email, trn_date)
 VALUES ('$username', '$password', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
@@ -45,47 +48,68 @@ VALUES ('$username', '$password', '$email', '$trn_date')";
     }else{
 ?>
 
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-md-7">
-            <h3>Register on <strong>Gold-dyx</strong></h3>
-            <p class="mb-4">You need to create an account in order to start investing.</p>
-            <form action="" method="post" name="registration">
-              <div class="form-group first">
-                <label for="username">Name</label>
-                <input type="text" class="form-control" placeholder="Name" id="username" name="username" required>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="img/logo-w-t-light.png" alt="logo">
               </div>
+              <h4>Hello! Register an account to get started</h4>
+              <h6 class="fw-light">Create an account.</h6>
+              <form action="" method="post" name="login" class="pt-3">
 
-               <div class="form-group first">
-                <label for="username">Email</label>
-                <input type="email" class="form-control" placeholder="Email" id="username" name="email" required>
-              </div>
+                 <div class="form-group">
+                  <input type="text" class="form-control form-control-lg" id="username" name="username" placeholder="Username">
+                </div>
 
-              <div class="form-group last mb-3">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" placeholder="Password" id="password" name="password" required>
-              </div>
-              
-            <div class="form-group first">
-                <label for="code">Referral Code</label>
-                <input type="text" class="form-control" placeholder="Referral Code" id="code" name="code" >
-              </div>
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="email" name="email" placeholder="Username">
+                </div>
 
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Password">
+                </div>
 
-              <input name="submit" type="submit" value="Register" class="btn btn-block btn-primary">
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-lg" id="code" name="code" placeholder="Referral Code">
+                </div>
 
-            </form>
-            <p>Already have an account? <a href='login.php'>Login</a></p>
+                <div class="mt-3">
+                  
+                  <input name="submit" type="submit" value="CREATE ACCOUNT" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
+                </div>
+               
+                
+                <div class="text-center mt-4 fw-light">
+                  Already have an account? <a href="login.php" class="text-primary">Sign In</a>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    
-    
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="../../vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/hoverable-collapse.js"></script>
+  <script src="../../js/template.js"></script>
+  <script src="../../js/settings.js"></script>
+  <script src="../../js/todolist.js"></script>
+  <!-- endinject -->
 
-    <script src="js1/jquery-3.3.1.min.js"></script>
-    <script src="js1/popper.min.js"></script>
-    <script src="js1/bootstrap.min.js"></script>
-    <script src="js1/main.js"></script>
-  <?php } ?>
+<?php } ?>
 </body>
 </html>
